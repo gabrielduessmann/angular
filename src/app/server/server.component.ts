2cc0be7627c8317b2815d84dc97693c6706387a9
Duessmann <gabriel.duessmann@gmail.com>
2021-02-10 23:47:43 -0300

    Creating a new component and using/caliing on HTML

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server', // for HTML files
+    templateUrl: './server.component.html' // HTML template for my component
+})
+export class ServerComponent {
+
+}
