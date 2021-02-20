5f082edfcac39c3d98ce0ab3f003c8375f83904e
Duessmann <gabriel.duessmann@gmail.com>
2021-02-20 10:54:35 -0300

    Ingredients model

---
+++ src/app/ingredients/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient {
+    
+    /*
+    public name: string;
+    public quantity: number;
+
+    constructor(name: string, quantity: number) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+    */
+
+    constructor(public name: string, public quantity: number)  { }
+
+}
