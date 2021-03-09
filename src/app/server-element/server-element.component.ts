ab5bfb5231e7df36801d6ca1d64b95470d7268d4
Duessmann <gabriel.duessmann@gmail.com>
2021-03-08 22:51:34 -0300

    Bidingin properties through different components with @Input()

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'] 
 })
 export class ServerElementComponent implements OnInit {
 
-  newServerName = '';
-  newServerContent = '';
+  @Input() element: {type: string, name: string, content: string};
   
   constructor() { }
 
