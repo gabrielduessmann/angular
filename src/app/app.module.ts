037f19c1a19e9704e17a153e6e382d88dad2cd41
Duessmann <gabriel.duessmann@gmail.com>
2021-03-22 22:10:45 -0300

    Creating an attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasicHighligtDirective } from './basic-highlight/basic.highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighligtDirective
   ],
   imports: [
     BrowserModule,
