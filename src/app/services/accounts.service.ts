2bccf8209348d3b83693de880890701b956547b1
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2021-05-19 22:37:16 -0300

    Injecting services into services with @Injectable

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,3 +1,8 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+// Injecatable is necessary here because of metadata and I don't have any other decorator
+@Injectable()
 export class AccountsService {
     accounts = [
         {
@@ -14,12 +19,17 @@ export class AccountsService {
         }
       ];
 
+      constructor(private loggingService: LoggingService) {}
+
       addAccount(name: string, status: string): void {
         this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status);
       }
 
       updateStatus(id: number, status: string): void {
-        if (this.accounts[id])
+        if (this.accounts[id]) {
             this.accounts[id].status = status;
+            this.loggingService.logStatusChange(status);
+        }
       }
 }
